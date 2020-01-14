c722c103dfd70bce43543e3d4568727fb04dcb30
Maxime <maxou3101@gmail.com>
2020-01-14 11:13:04 +0100

    logout

---
+++ groupeA7/groupeA7/src/main/java/be/helha/aemt/control/SessionControl.java
@@ -0,0 +1,13 @@
+package be.helha.aemt.control;
+
+import java.io.Serializable;
+
+import javax.servlet.http.HttpSession;
+
+public class SessionControl implements Serializable{
+
+	public void logout() {
+		/*HttpSession session = (HttpSession) */
+	}
+	
+}
