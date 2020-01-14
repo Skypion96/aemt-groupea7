19c36c622e1c490579a15845b580e76d283271d6
Maxime <maxou3101@gmail.com>
2020-01-14 19:00:32 +0100

    deconnexion ok

--- groupeA7/groupeA7/src/main/java/be/helha/aemt/control/SessionControl.java
+++ groupeA7/groupeA7/src/main/java/be/helha/aemt/control/SessionControl.java
@@ -3,9 +3,11 @@ package be.helha.aemt.control;
 import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
 @SessionScoped
@@ -14,11 +16,16 @@ public class SessionControl implements Serializable{
 
 	
 	public String logOut() {
-		FacesContext context = FacesContext.getCurrentInstance();
-        ExternalContext ec = context.getExternalContext();
-        final HttpServletRequest request = (HttpServletRequest) ec.getRequest();
-        request.getSession(false).invalidate(); // on invalide
-        return "index.xhtml";   
+		String page="/login?logout=true&faces-redirect=true";
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        try {
+          request.logout();
+        } catch (ServletException e) {
+            context.addMessage(null, new FacesMessage("Logout failed!"));
+            page="/login?logout=false&faces-redirect=true";
+        }
+        return page;  
     }
 
 	public boolean connected() {
