57e114815b7e7d9749fb5a937a2a8183be7d7f9c
Maxime <maxou3101@gmail.com>
2020-01-14 10:49:22 +0100

    delete js function no use

--- groupeA7/groupeA7/src/main/webapp/res/js/js.js
+++ groupeA7/groupeA7/src/main/webapp/res/js/js.js
@@ -3,9 +3,3 @@ function refresh()
     document.getElementById("txtMail").value = "";
     document.getElementById("txtMdp").value = "";
 }
-
-function keepContent() {
-	if(status == 302){
-		window.localStorage.setItem("co", "connecté");
-	}
-}
