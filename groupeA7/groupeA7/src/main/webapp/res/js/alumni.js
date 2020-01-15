dc1703448289fa3c07c7e2e37de1c177a4b39d65
Maxime <maxou3101@gmail.com>
2020-01-15 13:22:23 +0100

    search table

--- groupeA7/groupeA7/src/main/webapp/res/js/alumni.js
+++ groupeA7/groupeA7/src/main/webapp/res/js/alumni.js
@@ -1,5 +1,90 @@
-function alertme(){
-	if(status == 200){
-		alert("OK");
+//function sortTable(){
+//	var input, filter, table, tr, td, i, txtValue;
+//	  input = document.getElementById("search");
+//	  filter = input.value.toUpperCase();
+//	  table = document.getElementById("table");
+//	  tr = table.getElementsByTagName("tr");
+//	  
+//	  
+//	  for (i = 0; i < tr.length; i++) {
+//	    td = tr[i].getElementsByTagName("td")[0];
+//	    if (td) {
+//	      txtValue = td.textContent || td.innerText;
+//	      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//	        tr[i].style.display = "";
+//	      } else {
+//	        tr[i].style.display = "none";
+//	      }
+//	    }       
+//	  }
+//}
+
+function sortTable() {
+	var input, filter, table, tr, td, i, txtValue;
+	input = document.getElementById("search");
+	filter = input.value.toUpperCase();
+	table = document.getElementById("table");
+	tr = table.getElementsByTagName("tr");
+	for (i = 0; i < tr.length; i++) {
+		var abc = 0;
+		td = tr[i].getElementsByTagName("td")[0];
+		if (td) {
+			txtValue = td.textContent || td.innerText;
+			if (txtValue.toUpperCase().indexOf(filter) == 0) {
+				abc = 1;
+			} else {
+				// tr[i].style.display = "none";
+			}
+		} 
+		td1 = tr[i].getElementsByTagName("td")[1];
+		if (td1) {
+			txtValue = td1.textContent || td.innerText;
+			if (txtValue.toUpperCase().indexOf(filter) == 0) {
+				abc = 1;
+			} else {
+				//tr[i].style.display = "none";
+			}
+		}
+		if (abc == 1) {
+			tr[i].style.display = "";
+		}
+		else
+		{
+			tr[i].style.display = "none";
+		}
+		
+		td2 = tr[i].getElementsByTagName("td")[2];
+		if (td2) {
+			txtValue = td2.textContent || td.innerText;
+			if (txtValue.toUpperCase().indexOf(filter) == 0) {
+				abc = 1;
+			} else {
+				//tr[i].style.display = "none";
+			}
+		}
+		if (abc == 1) {
+			tr[i].style.display = "";
+		}
+		else
+		{
+			tr[i].style.display = "none";
+		}
+		
+		td4 = tr[i].getElementsByTagName("td")[4];
+		if (td4) {
+			txtValue = td4.textContent || td.innerText;
+			if (txtValue.toUpperCase().indexOf(filter) == 0) {
+				abc = 1;
+			} else {
+				//tr[i].style.display = "none";
+			}
+		}
+		if (abc == 1) {
+			tr[i].style.display = "";
+		}
+		else
+		{
+			tr[i].style.display = "none";
+		}
 	}
 }
